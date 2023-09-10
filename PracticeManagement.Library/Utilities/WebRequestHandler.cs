d6f8747f49f55010f0aea66989243ba3693d8c50
Oteo Mamo <oteomamo@gmail.com>
2023-09-10 17:49:20 -0400

    WebRequesstHendler Changes

--- PracticeManagement.Library/Utilities/WebRequestHandler.cs
+++ PracticeManagement.Library/Utilities/WebRequestHandler.cs
@@ -1,4 +1,5 @@
 ﻿using Newtonsoft.Json;
+using PracticeManagement.Library.Models;
 using System;
 using System.Net.Http;
 using System.Text;
@@ -12,9 +13,13 @@ namespace PracticeManagement.Library.Utilities
         private string host = "localhost";
         private string port = "5151";
         private HttpClient Client { get; }
+        private HttpProject Project { get; }
+        private HttpBill Bill { get; }
         public WebRequestHandler()
         {
             Client = new HttpClient();
+            Project = new HttpProject();
+            Bill = new HttpBill();
         }
         public async Task<string> Get(string url)
         {
@@ -95,7 +100,171 @@ namespace PracticeManagement.Library.Utilities
             }
 
         }
+         
+        /*    +++++++++++++++ Project Section    +++++++++++++    */
+        public async Task<string> GetProject(string url)
+        {
+            var fullUrl = $"http://{host}:{port}{url}";
+            try
+            {
+                using (var project = new HttpProject())
+                {
+                    var response = await project
+                        .GetStringAsync(fullUrl)
+                        .ConfigureAwait(false);
+                    return response;
+                }
+            }
+            catch (Exception e)
+            {
 
+            }
+
+
+            return null;
+        }
+
+        public async Task<string> DeleteProject(string url)
+        {
+            var fullUrl = $"http://{host}:{port}{url}";
+            try
+            {
+                using (var project = new HttpProject())
+                {
+                    using (var request = new HttpRequestMessage(HttpMethod.Delete, fullUrl))
+                    {
+                        using (var response = await project
+                                .SendAsync(request, HttpCompletionOption.ResponseHeadersRead)
+                                .ConfigureAwait(false))
+                        {
+                            if (response.IsSuccessStatusCode)
+                            {
+                                return await response.Content.ReadAsStringAsync();
+                            }
+                            return "ERROR";
+                        }
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+
+            }
+
+
+            return null;
+        }
+
+        public async Task<string> PostProject(string url, object obj)
+        {
+            var fullUrl = $"http://{host}:{port}{url}";
+            using (var project = new HttpProject())
+            {
+                using (var request = new HttpRequestMessage(HttpMethod.Post, fullUrl))
+                {
+                    var json = JsonConvert.SerializeObject(obj);
+                    using (var stringContent = new StringContent(json, Encoding.UTF8, "application/json"))
+                    {
+                        request.Content = stringContent;
+                        using (var response = await project
+                            .SendAsync(request, HttpCompletionOption.ResponseHeadersRead)
+                            .ConfigureAwait(false))
+                        {
+                            if (response.IsSuccessStatusCode)
+                            {
+                                return await response.Content.ReadAsStringAsync();
+                            }
+                            return "ERROR";
+                        }
+                    }
+                }
+            }
+
+        }
+
+        /*    +++++++++++++++   Bill Section    +++++++++++++    */
+
+        public async Task<string> GetBill(string url)
+        {
+            var fullUrl = $"http://{host}:{port}{url}";
+            try
+            {
+                using (var bill = new HttpBill())
+                {
+                    var response = await bill
+                        .GetStringAsync(fullUrl)
+                        .ConfigureAwait(false);
+                    return response;
+                }
+            }
+            catch (Exception e)
+            {
+
+            }
+
+
+            return null;
+        }
+
+        public async Task<string> DeleteBill(string url)
+        {
+            var fullUrl = $"http://{host}:{port}{url}";
+            try
+            {
+                using (var bill = new HttpBill())
+                {
+                    using (var request = new HttpRequestMessage(HttpMethod.Delete, fullUrl))
+                    {
+                        using (var response = await bill
+                                .SendAsync(request, HttpCompletionOption.ResponseHeadersRead)
+                                .ConfigureAwait(false))
+                        {
+                            if (response.IsSuccessStatusCode)
+                            {
+                                return await response.Content.ReadAsStringAsync();
+                            }
+                            return "ERROR";
+                        }
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+
+            }
+
+
+            return null;
+        }
+
+        public async Task<string> PostBill(string url, object obj)
+        {
+            var fullUrl = $"http://{host}:{port}{url}";
+            using (var bill = new HttpBill())
+            {
+                using (var request = new HttpRequestMessage(HttpMethod.Post, fullUrl))
+                {
+                    var json = JsonConvert.SerializeObject(obj);
+                    using (var stringContent = new StringContent(json, Encoding.UTF8, "application/json"))
+                    {
+                        request.Content = stringContent;
+                        using (var response = await bill
+                            .SendAsync(request, HttpCompletionOption.ResponseHeadersRead)
+                            .ConfigureAwait(false))
+                        {
+                            if (response.IsSuccessStatusCode)
+                            {
+                                return await response.Content.ReadAsStringAsync();
+                            }
+                            return "ERROR";
+                        }
+                    }
+                }
+            }
+
+        }
+
+        
 
     }
 }
